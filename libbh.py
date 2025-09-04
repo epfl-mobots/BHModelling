79a3ea828911157714d83c123236d9d8f38a591e
sirogo <cyril.sirogo@gmail.com>
2025-09-04 18:42:22 +0200

    implemented ABC frames

--- libbh.py
+++ libbh.py
@@ -6,7 +6,7 @@ import numpy as np
 from abc import ABC, abstractmethod
 
 # Helper to make a flat colored wall (surface)
-def make_wall(x, y, z, color="saddlebrown", opacity=0.5, name="Wall", showlegend:bool=False):
+def make_wall(x, y, z, color="saddlebrown", opacity=0.5, name:str="", legendgroup:str="", legendrank:int=1000, legendgrouptitle:dict={}, showlegend:bool=False):
     return go.Surface(
         x=x, y=y, z=z,
         surfacecolor=np.ones((2,2)),  # flat shading
@@ -14,8 +14,11 @@ def make_wall(x, y, z, color="saddlebrown", opacity=0.5, name="Wall", showlegend
         showscale=False,
         opacity=opacity,
         name=name,
-        colorscale=[[0, color], [1, color]],
-        showlegend=showlegend
+        legendrank = legendrank,
+        legendgroup = legendgroup,
+        legendgrouptitle = legendgrouptitle,
+        colorscale = [[0, color], [1, color]],
+        showlegend = showlegend
     )
 
 class PlotlyObject(ABC): # Abstract class
@@ -87,12 +90,20 @@ class ABC(Frame):
     n_subframes_y = 5           # Number of subframes along y
     n_subframes_z = 2           # Number of subframes along z
     
-    def __init__(self, x, height, depth, opacity):
+    def __init__(self, name:str, x, height, depth, opacity:float, position:int):
         super().__init__(x,height,depth,opacity)
+        
+        assert name.startswith("abc"), "Name of ABC frame must start with 'abc'"
+        self.name = name # Typically "abc21"
+        self.position = position
 
-    def draw_plotly(self, hive_height, hive_depth, color:str, show_legend:bool=False):
+    def draw_plotly(self, hive_height, hive_depth, show_legend:bool=False):
+        # Check that brood and honey data have been loaded
+        assert hasattr(self, 'brood_data'), "Brood data not loaded"
+        assert hasattr(self, 'honey_data'), "Honey data not loaded"
+
         # Create subframes
-        frame = []
+        subframes = []
         subframe_height = self.height / ABC.n_subframes_z
         subframe_depth = self.depth / ABC.n_subframes_y
         for i in range(ABC.n_subframes_z):
@@ -102,18 +113,40 @@ class ABC(Frame):
                 y0 = (hive_depth-self.depth)/2+ j * subframe_depth
                 y1 = (hive_depth-self.depth)/2+ (j + 1) * subframe_depth
 
+
+
+                htr_nb=int(ABC.htr_mapping[i][j][-1])
+                if self.brood_data[htr_nb] > 0:
+                    color = "blue"
+                    opacity=self.opacity
+                else:
+                    color = "yellow"
+                    opacity=self.honey_data[htr_nb]/200 if self.honey_data[htr_nb] <= 200 else 1.0 # Cap opacity at 1.0
+
                 subframe = make_wall(
                     x=[[self.x, self.x], [self.x, self.x]],
                     y=[[y0, y1], [y0, y1]],
                     z=[[z0, z0], [z1, z1]],
                     color=color,
-                    opacity=self.opacity,
+                    opacity=opacity,
                     name=ABC.htr_mapping[i][j],
+                    legendgroup=self.name,
+                    legendrank=2,
                     showlegend=show_legend
                 )
-                frame.append(subframe)
+                subframes.append(subframe)
 
-        return frame
+        # Modify the first subframe to change its legendgrouptitle to {text:self.name}
+        subframes[0].legendgrouptitle = {"text": f"frame_{self.position} ({self.name})"}
+
+        self.textures = subframes
+
+    def load_data(self, brood_data: list, honey_data: list):
+        """
+        Loads the brood and honey data into the ABC object.
+        """
+        self.brood_data = brood_data
+        self.honey_data = honey_data
 
 class BoxHive(PlotlyObject):
     def __init__(self, width, height, depth, opacity):
@@ -157,7 +190,7 @@ class BoxHive(PlotlyObject):
         # And store it in occupancy
         self.occupancy[position] = partition
 
-    def add_frame(self, position:int, opacity, show_legend:bool = True):
+    def add_frame(self, position:int, opacity:float, show_legend:bool = True):
         """
         Adds a frame into the hive at the give position
         """
@@ -169,13 +202,13 @@ class BoxHive(PlotlyObject):
         _x = self.frame_positioning[position]
         # Create the frame
         frame = Frame(x=_x, height=self.frame_height, depth=self.frame_depth, opacity=opacity)
-        frame.draw_plotly(self.height, self.depth, "gold", f"frame_{position}", show_legend)
+        frame.draw_plotly(self.height, self.depth, "brown", f"frame_{position}", show_legend)
         # And store it in occupancy
         self.occupancy[position] = frame
 
-    def add_abc(self, position:int, show_legend:bool = True):
+    def add_abc(self, position:int, opacity:float, name:str):
         """
-        Adds an ABC frame into the hive at the given position.
+        Adds an ABC frame into the hive at the given position and with the given opacity and name.
         """
         # First check that position is between 1 and 10:
         assert position >=1 and position <=10, "Position must be an integer comprised between [0,10]"
@@ -184,10 +217,21 @@ class BoxHive(PlotlyObject):
 
         _x = self.frame_positioning[position]
         # Create the ABC frame
-        abc = ABC(_x)
-        abc.draw_plotly(self.height, self.depth, "red", show_legend)
+        abc = ABC(name, _x, self.frame_height, self.frame_depth, opacity, position)
+        # And store it in occupancy
         self.occupancy[position] = abc
 
+    def loadABCdata(self, brood_data: dict, honey_data: dict):
+        """
+        Loads the brood and honey data into the ABC objects.
+        """
+        for abc in self.occupancy.values():
+            if isinstance(abc, ABC):
+                name = abc.name
+                assert name in brood_data.keys(), f"Brood data for {name} not found!"
+                assert name in honey_data.keys(), f"Honey data for {name} not found!"
+
+                abc.load_data(brood_data[name], honey_data[name])
 
     def getAllTextures(self):
         """
@@ -199,10 +243,11 @@ class BoxHive(PlotlyObject):
                 continue
             if not issubclass(type(self.occupancy[pos]), PlotlyObject):
                 raise(ValueError(f"Invalid object type in occupancy: {type(self.occupancy[pos])}, {self.occupancy[pos]}"))
+            if issubclass(type(self.occupancy[pos]), ABC):
+                self.occupancy[pos].draw_plotly(self.height, self.depth) # ABC textures computed at the very end
             all_textures.extend(self.occupancy[pos].get_textures())
 
         return all_textures
-
 
     def draw_plotly(self,show_legend:bool=False):
         """
@@ -216,21 +261,26 @@ class BoxHive(PlotlyObject):
             color="saddlebrown",
             opacity=self.opacity,
             name="Bottom",
+            legendgroup="hive",
+            legendrank=1,
             showlegend = show_legend
         )
 
-        # Front wall (y = 1 plane)  <-- swapped
+        # Front wall (y = 1 plane)
         front_wall = make_wall(
             x=[[0, self.width], [0, self.width]],
             y=[[0, 0], [0, 0]],
-            z=[[1, 1], [self.height, self.height]],
+            z=[[0.1*self.height, 0.1*self.height], [self.height, self.height]],
             color="peru",
             opacity=self.opacity,
             name="Front",
+            legendgroup="hive",
+            legendrank=1,
+            legendgrouptitle={"text": "Hive"},
             showlegend = show_legend
         )
 
-        # Back wall (y = hive_depth plane)  <-- swapped
+        # Back wall (y = hive_depth plane)
         back_wall = make_wall(
             x=[[0, self.width], [0, self.width]],
             y=[[self.depth, self.depth], [self.depth, self.depth]],
@@ -238,10 +288,12 @@ class BoxHive(PlotlyObject):
             color="peru",
             opacity=self.opacity,
             name="Back",
+            legendgroup="hive",
+            legendrank=1,
             showlegend = show_legend
         )
 
-        # Right wall (x = 0 plane)  <-- swapped
+        # Right wall (x = 0 plane)
         right_wall = make_wall(
             x=[[0, 0], [0, 0]],
             y=[[0, self.depth], [0, self.depth]],
@@ -249,10 +301,12 @@ class BoxHive(PlotlyObject):
             color="sienna",
             opacity=self.opacity,
             name="Right",
+            legendgroup="hive",
+            legendrank=1,
             showlegend = show_legend
         )
 
-        # Left wall (x = hive_width plane)  <-- swapped
+        # Left wall (x = hive_width plane)
         left_wall = make_wall(
             x=[[self.width, self.width], [self.width, self.width]],
             y=[[0, self.depth], [0, self.depth]],
@@ -260,6 +314,8 @@ class BoxHive(PlotlyObject):
             color="sienna",
             opacity=self.opacity,
             name="Left",
+            legendgroup="hive",
+            legendrank=1,
             showlegend = show_legend
         )
 
@@ -270,6 +326,8 @@ class BoxHive(PlotlyObject):
             color="black",
             opacity=self.opacity,
             name="Landing pad",
+            legendgroup="hive",
+            legendrank=1,
             showlegend = show_legend
         )
         self.textures = [bottom_wall, back_wall, front_wall, left_wall, right_wall, landing_pad]
